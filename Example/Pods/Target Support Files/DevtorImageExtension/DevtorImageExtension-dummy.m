1a387f68fdc48da5722bf80529464cb1f1f1a81e
Supakorn Thitivarangkool <ifeemily@gmail.com>
2019-07-31 16:11:44 +0700

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/DevtorImageExtension/DevtorImageExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DevtorImageExtension : NSObject
+@end
+@implementation PodsDummy_DevtorImageExtension
+@end
---
+++ Example/Pods/Target Support Files/Pods-DevtorImageExtension_Example/Pods-DevtorImageExtension_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevtorImageExtension_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DevtorImageExtension_Example
+@end
