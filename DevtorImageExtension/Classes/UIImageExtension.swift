e7bfb51489605c9aa561c3fed91834abc91ac222
Supakorn Thitivarangkool <ifeemily@gmail.com>
2019-08-02 11:54:27 +0700

    • Circle UIImageView included • UIImage customization included

---
+++ DevtorImageExtension/Classes/UIImageExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageExtension.swift
+//  DevtorImageExtension
+//
+//  Created by Supakorn Thitivarangkool on 2/8/2562 BE.
+//
+
+import Foundation
---
+++ DevtorImageExtension/Classes/UIImageViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageViewExtension.swift
+//  DevtorImageExtension
+//
+//  Created by Supakorn Thitivarangkool on 31/7/2562 BE.
+//
+
+import Foundation
